5c0ca24bdff5c339f2d704c38438e0498b1f1b9b
e-mita <ericmitaem@gmail.com>
2024-02-28 15:27:33 +0200

    (na/not-found-page): added new not found page

--- app/tickets/TicketList.tsx
+++ app/tickets/TicketList.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface Ticket {
@@ -23,11 +24,13 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket: Ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
